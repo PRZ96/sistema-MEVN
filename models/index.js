fe7c1aa821c5b63d6a1978213703fd6141246605
Pablo Perez <pablo.ceti.96@hotmail.com>
2023-01-24 17:09:20 -0600

    crear el modelo articulo

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 import Categoria from "./categoria"; //importamos el modelo categoria
+import Articulo from "./articulo"; //importamos el modelo articulo
 
 export default {
   Categoria, //Exportamos categoria
+  Articulo, //Exportamos articulo
 };
