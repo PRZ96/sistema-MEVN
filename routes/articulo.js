9a585e5ca148618594bfb92fbdfa44f8c344c29e
Pablo Perez <pablo.ceti.96@hotmail.com>
2023-02-10 14:40:50 -0600

    añadir restricciones a las rutas de categorias y articulos

--- routes/articulo.js
+++ routes/articulo.js
@@ -1,14 +1,16 @@
 import routerx from "express-promise-router";
 import articuloController from "../controllers/ArticuloController";
+import auth from "../middlewares/auth";
+
 const router = routerx();
 
 //Rutas para todas las funciones de nuestro controlador articuloController 
-router.post("/add", articuloController.add);
-router.get("/query", articuloController.query);
-router.get("/list", articuloController.list);
-router.put("/update", articuloController.update);
-router.delete("/remove", articuloController.remove);
-router.put("/activate", articuloController.activate);
-router.put("/deactivate", articuloController.deactivate);
+router.post("/add", auth.verifyAlmacenero, articuloController.add);
+router.get("/query", auth.verifyAlmacenero, articuloController.query);
+router.get("/list", auth.verifyAlmacenero, articuloController.list);
+router.put("/update", auth.verifyAlmacenero, articuloController.update);
+router.delete("/remove", auth.verifyAlmacenero, articuloController.remove);
+router.put("/activate", auth.verifyAlmacenero, articuloController.activate);
+router.put("/deactivate", auth.verifyAlmacenero, articuloController.deactivate);
 
 export default router;
