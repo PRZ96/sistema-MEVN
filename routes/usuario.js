efe386053a02ee9668f0a1b4dd8d70df4396aaaa
Pablo Perez <pablo.ceti.96@hotmail.com>
2023-02-10 14:31:01 -0600

    crear middleware para verificar roles y restringir las rutas solo para permitir al administrador

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,16 +1,17 @@
 import routerx from "express-promise-router";
 import usuarioController from "../controllers/UsuarioController";
+import auth from "../middlewares/auth";
 
 const router = routerx();
 
 //Rutas para todas las funciones de nuestro controlador usuarioController
-router.post("/add", usuarioController.add);
-router.get("/query", usuarioController.query);
-router.get("/list", usuarioController.list);
-router.put("/update", usuarioController.update);
-router.delete("/remove", usuarioController.remove);
-router.put("/activate", usuarioController.activate);
-router.put("/deactivate", usuarioController.deactivate);
+router.post("/add", auth.verifyAdministrador, usuarioController.add);
+router.get("/query", auth.verifyAdministrador, usuarioController.query);
+router.get("/list", auth.verifyAdministrador, usuarioController.list);
+router.put("/update", auth.verifyAdministrador, usuarioController.update);
+router.delete("/remove", auth.verifyAdministrador, usuarioController.remove);
+router.put("/activate", auth.verifyAdministrador, usuarioController.activate);
+router.put("/deactivate", auth.verifyAdministrador, usuarioController.deactivate);
 router.post("/login", usuarioController.login);
 
 export default router;
