a7e494d46710352facb9792c65df76d3162958e4
Pablo Perez <pablo.ceti.96@hotmail.com>
2023-02-17 19:18:31 -0600

    crear PersonaController y eliminar importacion no usada en controllers

--- controllers/ArticuloController.js
+++ controllers/ArticuloController.js
@@ -1,4 +1,3 @@
-import { model } from "mongoose";
 import models from "../models";
 
 export default {
--- controllers/PersonaController.js
+++ controllers/PersonaController.js
@@ -1,13 +1,10 @@
 import models from "../models";
-import bcrypt from "bcryptjs";
-import token from "../services/token";
 
 export default {
   add: async (req, res, next) => {
-    //permite agregar un usuario
+    //permite agregar una persona
     try {
-      req.body.password = await bcrypt.hash(req.body.password, 10); //Recibimos el valor de password, lo encriptamos y lo sobreescribimos en password.
-      const reg = await models.Usuario.create(req.body); //Recibe el objeto y crea el registro
+      const reg = await models.Persona.create(req.body); //Recibe el objeto y crea el registro
       res.status(200).json(reg); //Devuelvo un ok y el registro
     } catch (e) {
       res.status(500).send({
@@ -18,9 +15,9 @@ export default {
     }
   },
   query: async (req, res, next) => {
-    //consultar un usuario
+    //consultar una persona
     try {
-      const reg = await models.Usuario.findOne({ _id: req.query._id }); //Busca el usuario con el id enviado en el query con el parametro req
+      const reg = await models.Persona.findOne({ _id: req.query._id }); //Busca persona con el id enviado en el query con el parametro req
       if (!reg) {
         //Si no se encuentra el registro
         res.status(404).send({
@@ -38,10 +35,10 @@ export default {
     }
   },
   list: async (req, res, next) => {
-    //Enlistar usuario
+    //Enlistar persona
     try {
       let valor = req.query.valor;
-      const reg = await models.Usuario.find(
+      const reg = await models.Persona.find(
         {
           $or: [
             { nombre: new RegExp(valor, "i") },
@@ -58,26 +55,63 @@ export default {
       next(e);
     }
   },
-  update: async (req, res, next) => {
-    //Actualizar los datos de un usuario especifica
+  listClientes: async (req, res, next) => {
+    //Enlistar persona
     try {
-      let pas = req.body.password; //obtener password desde frontend
-      const reg0 = await models.Usuario.findOne({ _id: req.body._id }); //guardar el usuario como se encuentra actualmente en reg0
-      if (pas != reg0.password) {
-        //si el password enviado desde frontend es diferente al password actual, que encripte la nueva contraseña
-        req.body.password = await bcrypt.hash(req.body.password, 10);
-      }
-      const reg = await models.Usuario.findByIdAndUpdate(
+      let valor = req.query.valor;
+      const reg = await models.Persona.find(
+        {
+          $or: [
+            { nombre: new RegExp(valor, "i") },
+            { email: new RegExp(valor, "i") },
+          ],
+          tipo_persona: "Cliente",
+        }, //Agregamos que busque en el nombre o en descripcion
+        { createdAt: 0 }
+      ).sort({ createdAt: -1 }); //Ordenamos de forma descentente con -1, usar 1 para forma ascendente
+      res.status(200).json(reg);
+    } catch (e) {
+      res.status(500).send({
+        message: "Ocurrio un error",
+      });
+      next(e);
+    }
+  },
+  listProveedores: async (req, res, next) => {
+    //Enlistar persona
+    try {
+      let valor = req.query.valor;
+      const reg = await models.Persona.find(
+        {
+          $or: [
+            { nombre: new RegExp(valor, "i") },
+            { email: new RegExp(valor, "i") },
+          ],
+          tipo_persona: "Proveedor",
+        }, //Agregamos que busque en el nombre o en descripcion
+        { createdAt: 0 }
+      ).sort({ createdAt: -1 }); //Ordenamos de forma descentente con -1, usar 1 para forma ascendente
+      res.status(200).json(reg);
+    } catch (e) {
+      res.status(500).send({
+        message: "Ocurrio un error",
+      });
+      next(e);
+    }
+  },
+  update: async (req, res, next) => {
+    //Actualizar los datos de una persona especifica
+    try {
+      const reg = await models.Persona.findByIdAndUpdate(
         { _id: req.body._id }, //Parametro de busqueda
         {
-          rol: req.body.rol,
+          tipo_persona: req.body.tipo_persona,
           nombre: req.body.nombre,
           tipo_documento: req.body.tipo_documento,
           num_documento: req.body.num_documento,
           direccion: req.body.direccion,
           telefono: req.body.telefono,
           email: req.body.email,
-          password: req.body.password,
         } //Parametro de valores a cambiar en el registro
       );
       res.status(200).json(reg);
@@ -91,7 +125,7 @@ export default {
   remove: async (req, res, next) => {
     //Eliminar un registro
     try {
-      const reg = await models.Usuario.findByIdAndDelete({
+      const reg = await models.Persona.findByIdAndDelete({
         _id: req.body._id,
       });
       res.status(200).json(reg);
@@ -103,9 +137,9 @@ export default {
     }
   },
   activate: async (req, res, next) => {
-    //Activar un usuario desactivada
+    //Activar una Persona desactivada
     try {
-      const reg = await models.Usuario.findByIdAndUpdate(
+      const reg = await models.Persona.findByIdAndUpdate(
         { _id: req.body._id },
         { estado: 1 } //Valor a actualizar
       );
@@ -118,40 +152,13 @@ export default {
     }
   },
   deactivate: async (req, res, next) => {
-    //Desactivar un usuario activada
+    //Desactivar una Persona activada
     try {
-      const reg = await models.Usuario.findByIdAndUpdate(
+      const reg = await models.Persona.findByIdAndUpdate(
         { _id: req.body._id },
         { estado: 0 } //Valor a actualizar
       );
       res.status(200).json(reg);
-    } catch (e) {
-      res.status(500).send({
-        message: "Ocurrio un error",
-      });
-      next(e);
-    }
-  },
-  login: async (req, res, next) => {
-    try {
-      let user = await models.Usuario.findOne({ email: req.body.email, estado: 1 }); //Buscamos el usuario por el email ingresado y verificar que el usuario sea activo
-      if (user) {
-        //Si existe el usuario con ese email
-        let match = await bcrypt.compare(req.body.password, user.password); //Compara la contraseña ingresada con la actual
-        if (match) {
-          //Si coinciden las contraseñas
-          let tokenReturn = await token.encode(user._id); //Llamada a función token.encode enviando el _id de user
-          res.status(200).json({ user, tokenReturn });
-        } else {
-          res.status(404).send({
-            message: "Password incorrecto",
-          });
-        }
-      } else {
-        res.status(404).send({
-          message: "No existe el usuario",
-        });
-      }
     } catch (e) {
       res.status(500).send({
         message: "Ocurrio un error",
