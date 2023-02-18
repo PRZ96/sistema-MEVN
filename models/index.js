99f199c1e61184f8426e65d4bb0d9416b343db9e
Pablo Perez <pablo.ceti.96@hotmail.com>
2023-02-17 19:02:49 -0600

    crear el modelo Persona

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 import Categoria from "./categoria"; //importamos el modelo Categoria
 import Articulo from "./articulo"; //importamos el modelo Articulo
 import Usuario from "./usuario"; //importamos el modelo Usuario
+import Persona from "./persona"; //importamos el modelo Persona
 
 export default {
   Categoria, //Exportamos Categoria
   Articulo, //Exportamos Articulo
   Usuario, //Exportamos Usuario
+  Persona, //Exportamos Persona
 };
---
+++ models/persona.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+const personaSchema = new Schema({
+  tipo_persona: { type: String, maxlength: 20, required: true },
+  nombre: { type: String, maxlength: 50, unique: true, required: true },
+  tipo_documento: { type: String, maxlength: 20 },
+  num_documento: { type: String, maxlength: 20 },
+  direccion: { type: String, maxlength: 70 },
+  telefono: { type: String, maxlength: 20 },
+  email: { type: String, maxlength: 50, unique: true },
+  estado: { type: Number, default: 1 },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Persona = mongoose.model("persona", personaSchema);
+export default Persona;
