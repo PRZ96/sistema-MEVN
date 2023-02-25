80bbbae3eaf20f8f47b93330f1ddeede60bc3712
Pablo Perez <pablo.ceti.96@hotmail.com>
2023-02-25 11:17:15 -0600

    crear el modelo Ingreso

--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ import Categoria from "./categoria"; //importamos el modelo Categoria
 import Articulo from "./articulo"; //importamos el modelo Articulo
 import Usuario from "./usuario"; //importamos el modelo Usuario
 import Persona from "./persona"; //importamos el modelo Persona
+import Ingreso from "./ingreso"; //importamos el modelo Ingreso
 
 export default {
   Categoria, //Exportamos Categoria
   Articulo, //Exportamos Articulo
   Usuario, //Exportamos Usuario
   Persona, //Exportamos Persona
+  Ingreso, //Exportamos Ingreso
 };
---
+++ models/ingreso.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+
+const ingresoSchema = new Schema({
+  usuario: { type: Schema.ObjectId, ref: "usuario", required: true },
+  persona: { type: Schema.ObjectId, ref: "persona", required: true },
+  tipo_comprobante: { type: String, maxlength: 20, required: true },
+  serie_comprobante: { type: String, maxlength: 7 },
+  num_comprobante: { type: String, maxlength: 10, required: true },
+  impuesto: { type: Number, required: true },
+  total: { type: Number, required: true },
+  detalles: [{
+      _id: {
+        type: String,
+        required: true,
+      },
+      articulo: {
+        type: String,
+        required: true,
+      },
+      cantidad: {
+        type: Number,
+        required: true,
+      },
+      precio: {
+        type: Number,
+        required: true,
+      },
+    }],
+  estado: { type: Number, default: 1 },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Ingreso = mongoose.model("ingreso", ingresoSchema);
+export default ingreso;
