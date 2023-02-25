0eb74f992973996bd4c3f1faf3e554345df3bf3e
Pablo Perez <pablo.ceti.96@hotmail.com>
2023-02-25 11:33:33 -0600

    crear IngresoController

---
+++ controllers/IngresoController.js
@@ -0,0 +1,117 @@
+import models from "../models";
+
+export default {
+  add: async (req, res, next) => {
+    //permite agregar un ingreso
+    try {
+      const reg = await models.Ingreso.create(req.body); //Recibe el objeto y crea el registro
+      res.status(200).json(reg); //Devuelvo un ok y el registro
+    } catch (e) {
+      res.status(500).send({
+        //Devolvemos bad request y enviamos en un array el texto de error
+        message: "Ocurrio un error",
+      });
+      next(e); //Devolvemos el error con morgan
+    }
+  },
+  query: async (req, res, next) => {
+    //consultar una categoria
+    try {
+      const reg = await models.Ingreso.findOne({ _id: req.query._id }) //Busca el ingreso con el id enviado en el query con el parametro req
+        .populate("usuario", { nombre: 1 })
+        .populate("persona", { nombre: 1 });
+      if (!reg) {
+        //Si no se encuentra el registro
+        res.status(404).send({
+          //respondemos con error de no encontrado y el mensaje
+          message: "El registro no existe",
+        });
+      } else {
+        res.status(200).json(reg); //Devolvemos ok
+      }
+    } catch (e) {
+      res.status(500).send({
+        message: "Ocurrio un error",
+      });
+      next(e);
+    }
+  },
+  list: async (req, res, next) => {
+    //Enlistar ingresos
+    try {
+      let valor = req.query.valor;
+      const reg = await models.Ingreso.find(
+        {$or:[{ num_comprobante: new RegExp(valor, "i") }, { serie_comprobante: new RegExp(valor, "i") }]}, //Agregamos que busque en el numero de comprobante o en serie del comprobante
+        { createdAt: 0 })
+        .populate("usuario", { nombre: 1 })
+        .populate("persona", { nombre: 1 })
+        .sort({ createdAt: -1 }); //Ordenamos de forma descentente con -1, usar 1 para forma ascendente
+      res.status(200).json(reg);
+    } catch (e) {
+      res.status(500).send({
+        message: "Ocurrio un error",
+      });
+      next(e);
+    }
+  },
+  /* Como no se deben eliminar o modificar los ingresos, deshabilito las siguientes funciones update y remove */
+/*   update: async (req, res, next) => {
+    //Actualizar los datos de un ingreso especifico
+    try {
+      const reg = await models.Ingreso.findByIdAndUpdate(
+        { _id: req.body._id }, //Parametro de busqueda
+        { nombre: req.body.nombre, descripcion: req.body.descripcion } //Parametro de valores a cambiar en el registro
+      );
+      res.status(200).json(reg);
+    } catch (e) {
+      res.status(500).send({
+        message: "Ocurrio un error",
+      });
+      next(e);
+    }
+  },
+  remove: async (req, res, next) => {
+    //Eliminar un ingreso
+    try {
+      const reg = await models.Ingreso.findByIdAndDelete({
+        _id: req.body._id,
+      });
+      res.status(200).json(reg);
+    } catch (e) {
+      res.status(500).send({
+        message: "Ocurrio un error",
+      });
+      next(e);
+    }
+  }, */
+  activate: async (req, res, next) => {
+    //Activar una ingreso desactivada
+    try {
+      const reg = await models.Ingreso.findByIdAndUpdate(
+        { _id: req.body._id },
+        { estado: 1 } //Valor a actualizar
+      );
+      res.status(200).json(reg);
+    } catch (e) {
+      res.status(500).send({
+        message: "Ocurrio un error",
+      });
+      next(e);
+    }
+  },
+  deactivate: async (req, res, next) => {
+    //Desactivar un ingreso activada
+    try {
+      const reg = await models.Ingreso.findByIdAndUpdate(
+        { _id: req.body._id },
+        { estado: 0 } //Valor a actualizar
+      );
+      res.status(200).json(reg);
+    } catch (e) {
+      res.status(500).send({
+        message: "Ocurrio un error",
+      });
+      next(e);
+    }
+  },
+};
