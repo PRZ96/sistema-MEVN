d4d09e7f765a0a27cf25c01f180d209721bff88a
Pablo Perez <pablo.ceti.96@hotmail.com>
2023-04-04 14:59:53 -0600

    validar los campos de texto de crear categoria

--- routes/categoria.js
+++ routes/categoria.js
@@ -5,9 +5,13 @@ import auth from "../middlewares/auth";
 const router = routerx();
 
 //Rutas para todas las funciones de nuestro controlador categoriaController
-router.post("/add", auth.verifyAlmacenero, categoriaController.add);
+//router.post("/add", auth.verifyAlmacenero, categoriaController.add);
+router.post("/add", categoriaController.add);
+
 router.get("/query", auth.verifyAlmacenero, categoriaController.query);
-router.get("/list", auth.verifyAlmacenero, categoriaController.list);
+//router.get("/list", auth.verifyAlmacenero, categoriaController.list);
+router.get("/list", categoriaController.list); //ruta temporal en lo que asigno accesos
+
 router.put("/update", auth.verifyAlmacenero, categoriaController.update);
 router.delete("/remove", auth.verifyAlmacenero, categoriaController.remove);
 router.put("/activate", auth.verifyAlmacenero, categoriaController.activate);
